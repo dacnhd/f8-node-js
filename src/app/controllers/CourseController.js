376535163f3679499ceafa138dcff5fc10d0273a
dacnhd <dacnhd@fpt.edu.vn>
2023-04-26 18:03:22 +0700

    Soft delete.

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -34,14 +34,27 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
-        const course = new Course(formData)
-        course.save().then(() => res.redirect('/')).catch(next)
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(req.body)
+        course.save().then(() => res.redirect('/me/stored/courses')).catch(next)
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(res.redirect('back'))
+            .catch(next)
     }
 
     // [DELETE] /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(res.redirect('back'))
+            .catch(next)
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
             .then(res.redirect('back'))
             .catch(next)
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const slug = require('mongoose-slug-generator')
-mongoose.plugin(slug)
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema({
     name: { type: String, required: true },
@@ -13,6 +13,13 @@ const Course = new Schema({
 }, {
     // bằng với việc tạo createdAt và updatedAt
     timestamps: true
+});
+
+// Add plugin
+mongoose.plugin(slug)
+Course.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all'
 });
 
 module.exports = mongoose.model('Course', Course)
